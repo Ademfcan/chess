e8a077f645df2aa6a0b66ef8cf49e21594b1d1d7
AdemCan <ademfcan@gmail.com>
2026-01-04 03:53:21 -0800

    Add skeletal message handler, types and payload placeholders for client/server

--- src/serverside/src/main/java/chessserver/Net/PayloadTypes/MetricMessagePayloadTypes.java
+++ src/serverside/src/main/java/chessserver/Net/PayloadTypes/MetricMessagePayloadTypes.java
@@ -1,1 +1,4 @@
 package chessserver.Net.PayloadTypes;
+
+public class MetricMessagePayloadTypes {
+}
---
+++ src/serverside/src/main/java/chessserver/Net/PayloadTypes/SkeletonMessagePayloadTypes.java
@@ -0,0 +1,7 @@
+//package chessserver.Net.PayloadTypes;
+//
+//import chessserver.Net.Payload;
+//
+//public class SkeletonMessagePayloadTypes {
+//    public record Payload1(String a, Integer b) implements Payload {} ....
+//}
